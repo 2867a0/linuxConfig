ad3913d5ec57b2cfb928ec1074d14292109c3c2d
2867a0 <l1969758113@126.com>
2021-07-17 20:57:52 +0800

    update background code

--- scripts/autochangetheme.sh
+++ scripts/autochangetheme.sh
@@ -2,12 +2,12 @@
 time=`date | cut -d " " -f 5`;
 
 if [ ${time%%:*} == 7 ]; then
-	gsettings set org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/colorado.jpg';
+	gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/Coast&sunset..jpg';
 	gsettings set org.gnome.desktop.interface gtk-theme 'Mojave-light';
 	gsettings set org.gnome.shell.extensions.user-theme name 'Mojave-light';
 elif [ ${time%%:*} == 18 ]; then
 	# background
-	gsettings set org.gnome.desktop.screensaver picture-uri 'file:///usr/share/backgrounds/colorado.jpg';
+	gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/colorado.jpg';
 	# application
 	gsettings set org.gnome.desktop.interface gtk-theme 'Mojave-dark';
 	# shell
