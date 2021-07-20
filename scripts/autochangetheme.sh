659169c95492b07a59f0f9be6a292fba26cdd981
2867a0 <l1969758113@126.com>
2021-07-21 06:50:44 +0800

    update

--- scripts/autochangetheme.sh
+++ scripts/autochangetheme.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 time=`date | cut -d " " -f 5`;
 
-if [ ${time%%:*} == 7 ]; then
+if [ ${time%%:*} == 07 ]; then
 	gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/Coast&sunset..jpg';
 	gsettings set org.gnome.desktop.interface gtk-theme 'Mojave-light';
 	gsettings set org.gnome.shell.extensions.user-theme name 'Mojave-light';
