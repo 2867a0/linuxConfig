25cd6071c1a8a187ff74c0d78f725f98a9a3aafe
2867a0 <l1969758113@126.com>
2021-08-08 09:05:51 +0800

    UPDATE SCRIPT

--- scripts/autochangetheme.sh
+++ scripts/autochangetheme.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-time=`date | cut -d " " -f 5`;
 
-if [ ${time%%:*} == 07 ]; then
+light=("07" "08" "09" "10" "11" "12" "13" "14" "15" "16" "17" "18" "19");
+
+date=`date | cut -d " " -f 5`;
+time=${date%%:*};
+
+# light
+#if [[ $time -ge 07 && $time -le 19 ]]; then
+if [[ "${light[@]}" =~ "${time}"  ]]; then
 	gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/Coast&sunset..jpg';
 	gsettings set org.gnome.desktop.interface gtk-theme 'Mojave-light';
 	gsettings set org.gnome.shell.extensions.user-theme name 'Mojave-light';
-elif [ ${time%%:*} == 18 ]; then
+# night
+else
 	# background
 	gsettings set org.gnome.desktop.background picture-uri 'file:///usr/share/backgrounds/colorado.jpg';
 	# application
